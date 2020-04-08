f559a2cf77563aa0b15b5b85caff36e022729285
tamal <tamal.sen@hotmail.com>
2020-04-08 23:27:17 +0400

    url

--- personal/urls.py
+++ personal/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    path('',views.homepage, name= 'home' ),    
+    url('',views.homepage, name= 'home' ),    
 ]
